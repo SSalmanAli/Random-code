322241af5a424c1facea859809a4e0fe976dd6b9
SSalmanAli <159621448+SSalmanAli@users.noreply.github.com>
2024-03-05 17:23:10 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+// "use strict";
+// let Name = ("Salman Ali");
+// let age = 17;
+// let isMarried = false;
+// let LastName = "Ali";
+// let x = [Name, age, isMarried, LastName];
+// x.forEach(y => console.log(y, "= is in my Cnic"));
+
+// let x = 1 ;
+// let y = "Salman" ;
+
+// let result = x / y ;
+
+// console.log(result);
+
+// let x = 5 ;
+// let y = 2 ;         //modulus
+// console.log (x ** y) ;
